3ac4fa81e356c7c394bfaa35ffcfe815c64be937
kongjun0320 <1741142595@qq.com>
2022-01-19 09:48:29 +0800

    feat: hello-webpack

---
+++ hello-webpack/webpack.config.js
@@ -0,0 +1,10 @@
+const { resolve } = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist')
+  }
+}
